42e3e9c1d953cce9755a68ff5b9ede88f71fca5c
Ryan Oldenburg <ryan@guzba.com>
2024-01-18 01:59:45 -0600

    check arc/orc

--- src/ready.nim
+++ src/ready.nim
@@ -2,6 +2,10 @@ import ready/connections
 export connections
 
 when compileOption("threads"):
+  when not defined(nimdoc):
+    when not defined(gcArc) and not defined(gcOrc):
+      {.error: "Ready requires --mm:arc or --mm:orc when --threads:on.".}
+
   ## Using the connection pool requires --threads:on
   import ready/pools
   export pools
